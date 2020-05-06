0262c75066866c1a6ce08150a20f1b35272a064a
piteli <rushwender@gmail.com>
2020-05-07 05:51:44 +0800

    updated

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -18,6 +18,25 @@ class _DetailsPageState extends State<DetailsPage> {
       appBar : new AppBar(
         title : new Text("Post Details"),
         backgroundColor : Colors.green
+      ),
+      body : new ListView(
+        children : <Widget>[
+          new Container(
+            margin : EdgeInsets.all(10.0),
+            child : new Row(
+              children : <Widget>[
+                new CircleAvatar(
+                  child: new Text(widget.snapshot.data['title'][0]),
+                  backgroundColor : Colors.green,
+                  foregroundColor : Colors.white,
+                ),
+                new SizedBox(width : 10.0,),
+                new Text(widget.snapshot.data["title"]),
+                style : TextStyle(fontSize : 21.0, color : Colors.green)
+              ]
+            )
+          )
+        ]
       )
     );
   }
